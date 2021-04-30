1fd3477b72543753040846bf76d4c266a65e37dd
JoncarlosT <40675955+JoncarlosT@users.noreply.github.com>
2021-04-30 11:40:02 -0400

    test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "100mb" }));
 app.use(cookieParser());
 app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
 
