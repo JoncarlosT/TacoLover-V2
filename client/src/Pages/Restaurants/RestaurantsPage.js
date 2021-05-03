98203b23fa6253434276a704957a42d1fc5dfe68
JoncarlosT <40675955+JoncarlosT@users.noreply.github.com>
2021-05-03 19:20:38 -0400

    Map Fix

--- client/src/Pages/Restaurants/RestaurantsPage.js
+++ client/src/Pages/Restaurants/RestaurantsPage.js
@@ -1,8 +1,6 @@
 import React, { memo, useEffect, useState } from "react";
-import ReactMapboxGl, { Marker, Popup, Layer } from "react-mapbox-gl";
-import * as MapboxGl from "mapbox-gl";
+import ReactMap, { Marker, Popup } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-
 import DevLocalHost from "../../GlobalProvider";
 
 import axios from "axios";
@@ -12,9 +10,13 @@ import { RestaurantsWrapper, TacoIcon } from "./styles";
 
 export default function RestaurantsPage() {
   //SETTING UP MAP
-  const Map = ReactMapboxGl({
-    accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
-    trackResize: true,
+  //SETTING UP MAP
+  const [viewport, setViewport] = useState({
+    latitude: 40.66995747013945,
+    longitude: -103.59179687498357,
+    zoom: 3,
+    width: "100vw",
+    height: "500px",
   });
 
   //GETS RESTUARANTS
@@ -34,19 +36,19 @@ export default function RestaurantsPage() {
 
   return (
     <div>
-      <Map
-        style="mapbox://styles/jc1280/ckm16fh6i9x6n17o0pm4r5091"
-        center={[-103.59179687498357, 40.66995747013945]}
-        zoom={[3]}
-        containerStyle={{
-          height: "calc(100vh - 350px)",
-          width: "100%",
-        }}
+      <ReactMap
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/jc1280/ckm16fh6i9x6n17o0pm4r5091"
+        onViewportChange={(viewport) => setViewport(viewport)}
       >
         {restaurants.map((restaurant, i) => {
-          console.log(restaurant);
           return (
-            <Marker key={i} coordinates={restaurant.geometry.coordinates}>
+            <Marker
+              key={i}
+              latitude={restaurant.geometry.coordinates[1]}
+              longitude={restaurant.geometry.coordinates[0]}
+            >
               <TacoIcon
                 onClick={(e) => {
                   e.preventDefault();
@@ -61,15 +63,17 @@ export default function RestaurantsPage() {
 
         {selectedTaco && (
           <Popup
+            onClick={console.log("hit")}
             onClose={() => setSelectedTaco(null)}
-            coordinates={selectedTaco.geometry.coordinates}
+            latitude={selectedTaco.geometry.coordinates[1]}
+            longitude={selectedTaco.geometry.coordinates[0]}
           >
             <h3>{selectedTaco.title}</h3>
             <p>{selectedTaco.description}</p>
             <p>By: {selectedTaco.author.userName}</p>
           </Popup>
         )}
-      </Map>
+      </ReactMap>
       <RestaurantsWrapper></RestaurantsWrapper>
     </div>
   );
