4e585aeebd74eb99f88b28226860be0ca92ae7d7
JoncarlosT <40675955+JoncarlosT@users.noreply.github.com>
2021-05-03 18:11:43 -0400

    Updating map

--- client/src/Pages/Restaurants/RestaurantsPage.js
+++ client/src/Pages/Restaurants/RestaurantsPage.js
@@ -1,56 +1,71 @@
-import React, { memo, useState } from "react";
-import ReactMap, { Marker, Popup } from "react-map-gl";
+import React, { memo, useEffect, useState } from "react";
+import ReactMapboxGl, { Marker, Popup, Layer } from "react-mapbox-gl";
+import * as MapboxGl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+import DevLocalHost from "../../GlobalProvider";
+
+import axios from "axios";
+
+//IMPORT STYLES
+import { RestaurantsWrapper, TacoIcon } from "./styles";
 
 export default function RestaurantsPage() {
   //SETTING UP MAP
-  const [viewport, setViewport] = useState({
-    latitude: 40.66995747013945,
-    longitude: -103.59179687498357,
-    zoom: 3,
-    width: "100vw",
-    height: "500px",
+  const Map = ReactMapboxGl({
+    accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
+    trackResize: true,
   });
+
+  //GETS RESTUARANTS
+  const [restaurants, setRestaurants] = useState([]);
+
+  const getRestaurants = async () => {
+    axios
+      .get(DevLocalHost() + "/restaurants")
+      .then((res) => setRestaurants(res.data));
+  };
+
+  useEffect(() => {
+    getRestaurants();
+  }, []);
+
+  const [selectedTaco, setSelectedTaco] = useState(null);
 
   return (
     <div>
-      <ReactMap
-        {...viewport}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/jc1280/ckm16fh6i9x6n17o0pm4r5091"
-        onViewportChange={(viewport) => setViewport(viewport)}
-      />
+      <Map
+        style="mapbox://styles/jc1280/ckm16fh6i9x6n17o0pm4r5091"
+        center={[-103.59179687498357, 40.66995747013945]}
+        zoom={[3]}
+        containerStyle={{
+          height: "calc(100vh - 350px)",
+          width: "100%",
+        }}
+      >
+        {restaurants.map((restaurant, i) => {
+          console.log(restaurant);
+          return (
+            <Marker key={i} coordinates={restaurant.geometry.coordinates}>
+              <TacoIcon
+                onClick={(e) => {
+                  e.preventDefault();
+                  setSelectedTaco(restaurant);
+                }}
+                src="/Assets/Taco-svg.png"
+                alt="taco"
+              />
+            </Marker>
+          );
+        })}
 
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
-      <h1>Helo</h1>
+        {selectedTaco && (
+          <Popup coordinates={selectedTaco.geometry.coordinates}>
+            <h1>hello</h1>
+          </Popup>
+        )}
+      </Map>
+      <RestaurantsWrapper></RestaurantsWrapper>
     </div>
   );
 }
---
+++ client/src/Pages/Restaurants/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const RestaurantsWrapper = styled.div`
+  margin-top: 30px;
+`;
+
+export const TacoIcon = styled.img`
+  height: 30px;
+  width: 30px;
+  cursor: pointer;
+`;
