bde368bd9f43033a6da79dc321b0ced127ff49ae
JoncarlosT <40675955+JoncarlosT@users.noreply.github.com>
2021-05-04 17:34:30 -0400

    Test of Mobile background fix

--- client/src/styles.js
+++ client/src/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import BG from "./Assets/Taco-bg.jpg";
 
 export const RouterWrapper = styled.div`
-  background-image: url(${BG});
+  /* background-image: url(${BG});
   background-attachment: fixed;
-  background-size: cover;
+  background-size: cover; */
 `;
