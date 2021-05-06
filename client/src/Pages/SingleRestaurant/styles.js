42ed92325093072b9296790aee298169be4fd41c
JoncarlosT <40675955+JoncarlosT@users.noreply.github.com>
2021-05-05 20:00:28 -0400

    save

---
+++ client/src/Pages/SingleRestaurant/styles.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const StyledSingleRestaurant = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  background: rgba(0, 0, 0, 0.5);
+`;
+
+export const RestaurantSide = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const RestaurantTitle = styled.h1`
+  color: white;
+  font-size: 3rem;
+  margin: 20px;
+`;
+
+export const RestaurantImg = styled.img`
+  height: 400px;
+  width: 400px;
+`;
+
+export const RestaurantText = styled.h1`
+  color: white;
+`;
+
+export const CommentSide = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const CommentHeader = styled.h1`
+  color: white;
+  font-size: 3rem;
+  margin: 20px;
+`;
