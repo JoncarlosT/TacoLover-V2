b608547ceffed522d27c579903fee055e3613b4b
JoncarlosT <40675955+JoncarlosT@users.noreply.github.com>
2021-05-19 21:53:03 -0400

    create rest fix

--- client/src/Pages/CreateRestaurant/styles.js
+++ client/src/Pages/CreateRestaurant/styles.js
@@ -6,6 +6,7 @@ export const RestaurantForm = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
+  height: 85vh;
 `;
 
 export const RestaurantFormInput = styled.textarea`
