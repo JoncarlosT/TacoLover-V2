295a505ed84612b06a5f9e7c235d471fe202b2c9
JoncarlosT <40675955+JoncarlosT@users.noreply.github.com>
2021-05-20 22:07:28 -0400

    herkou fix

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const cors = require("cors");
 
 dotenv.config();
 
+//SETTING UP HEROKU
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "client/build/index.html"));
+  });
+}
+
 //SETTING UP SERVER
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -36,12 +45,3 @@ mongoose.connect(
 app.use("/restaurants", require("./routers/restaurantsRouter"));
 app.use("/authentication", require("./routers/userRouter"));
 app.use("/review", require("./routers/reviewRouter"));
-
-//SETTING UP HEROKU
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "client/build/index.html"));
-  });
-}
